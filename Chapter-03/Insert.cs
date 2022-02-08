9245965e5053e1c56cfd57a3ac5e65c5069000d7
Guilherme Rocha <GuiPython2001@gmail.com>
2022-02-08 13:52:29 -0300

    update: Module 3 Handling Events

---
+++ Chapter-03/Insert.cs
@@ -0,0 +1,40 @@
+using Program;
+
+namespace Events
+{
+    public class InsertEventArgs
+    {
+        public string Query { get; set; }
+
+        public InsertEventArgs(string query)
+        {
+            Query = query;
+        }
+    }
+    public class Insert
+    {
+        public event EventHandler<InsertEventArgs>? InsertPerformed;
+        public event EventHandler? InsertCompleted;
+
+        protected virtual void OnInsertPerformed(string query)
+        {
+            if (InsertPerformed != null)
+                InsertPerformed(this, new InsertEventArgs(query));
+        }
+
+        protected virtual void OnInsertCompleted()
+        {
+            if (InsertCompleted != null)
+                InsertCompleted(this, EventArgs.Empty);
+        }
+
+        public void Run(Queue<string> queries)
+        {
+            while (queries.Count() > 0)
+            {
+                OnInsertPerformed(queries.Dequeue());
+            }
+            OnInsertCompleted();
+        }
+    }
+}
